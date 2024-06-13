3014c6e4284669ef43fafd406eee0004f376aaf5
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-06-13 11:04:41 +0700

    jika parent is null tree kosong tandai root menjadi new node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -36,7 +36,11 @@ public:
         Node* currentNode = nullptr;
         search(element, parent, currentNode); // locate the node which will be the parent of the node to be insert
 
-        
+        if (parent == nullptr) // if the parent is NULL (tree is empty)
+        {
+            ROOT = newNode; // mark the new node as root
+            return; // exit
+        }
     }
 };
 
